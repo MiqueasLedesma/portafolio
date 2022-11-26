c0e524d2db2da589e14bc74dbc08d54cc7ab6d3e
miqueasledesma <mickeas98@gmail.com>
2022-11-26 19:12:36 -0300

    update almost ready

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 import send_logo from '../images/send-logo.png';
 const ContactContainer = styled.div`
@@ -34,20 +34,52 @@ const ContactContainer = styled.div`
 `
 
 export const Contact = () => {
+
+    const [message, setMessage] = useState("");
+    const [email, setEmail] = useState("");
+    const [name, setName] = useState("");
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        await fetch(e.target.action, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                message,
+            }),
+        });
+        setMessage("")
+        setEmail("")
+        setName("")
+        alert("Form submitted");
+    };
+
     return (
         <ContactContainer>
             <h2>Contact:</h2>
-            <form>
-                <label>Name</label>
-                <input className='name' type="text" />
-                <label>Email</label>
-                <input className='email' type="text" />
-                <label>Message</label>
-                <textarea name="message" id="" cols="30" rows="10"></textarea>
+            <form action="https://submit-form.com/fSutHLJe" onSubmit={handleSubmit}>
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" placeholder="Name" required="" value={name} onChange={e => setName(e.target.value)} />
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Email" required="" value={email} onChange={e => setEmail(e.target.value)} />
+                <label for="message">Message</label>
+                <textarea
+                    value={message}
+                    onChange={e => setMessage(e.target.value)}
+                    cols="30"
+                    rows="10"
+                    id="message"
+                    name="message"
+                    placeholder="Message"
+                    required=""
+                ></textarea>
                 <div className='box'>
-                    <button>
-                        <img src={send_logo} alt="" />
-                    </button>
+                    <button type="submit"><img src={send_logo} alt="" /></button>
                 </div>
             </form>
         </ContactContainer>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,17 +3,28 @@ import styled from 'styled-components';
 import sun_logo from '../images/sun-logo.svg';
 import moon_logo from '../images/moon-logo.svg';
 import { selectLanguage } from '../Languages';
+import { MenuIcon } from './MenuIcon';
 
 const NavContainer = styled.div`
     position: fixed;
     z-index: 1;
     width: 100%;
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
     height: fit-content;
     background-color: ${props => localStorage.theme == 'primary' ? props.theme.primary : props.theme.secundary};
     color: ${props => localStorage.theme == 'primary' ? props.theme.textPrimary : props.theme.textSecundary};
+    .bigBox {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+    }
+    .bigBox.active {
+        display: grid;
+        grid-template-columns: repeat(5, 1fr);
+        grid-template-rows: repeat(5, 1fr);
+        grid-column-gap: 0px;
+        grid-row-gap: 0px; 
+    }
     button {
         display: none;
         @media screen and (max-width:768px) {
@@ -42,6 +53,9 @@ const NavContainer = styled.div`
             margin-right: 1rem;
         }
     }
+    .box.active {
+        grid-area: 1 / 5 / 2 / 6;
+    }
     .links {
         display: flex;
         user-select: none;
@@ -59,71 +73,81 @@ const NavContainer = styled.div`
         }
     }
     .links.active {
-        padding-top: 2rem;
         display: flex;
         flex-direction: column;
-        height: fit-content;
-        gap: 20px;
+        align-items: center;
+        grid-area: 2 / 3 / 6 / 4;
+        @media screen and (min-width:768px){
+            display: none;
+        }
     }
+    .menu {
+        @media screen and (min-width:768px){
+            display: none;
+        }
+    }
+    .menu.active {
+        grid-area: 1 / 1 / 2 / 2;
+    }
+    
 `
 
 export const Navbar = ({ handleClick, handleSelect, language }) => {
 
     const { nav_text } = selectLanguage(language);
 
-    const [state, setState] = useState({});
     const [open, setOpen] = useState(false);
 
     const handleScroll = e => {
         window.scrollTo(0, 2200);
-        setState({ [e.target.id]: true })
     }
 
     const handleScroll2 = e => {
         window.scrollTo(0, 0);
-        setState({ [e.target.id]: true })
     }
 
     const handleScroll3 = e => {
         window.scrollTo(0, 400);
-        setState({ [e.target.id]: true })
     }
 
     const handleScroll4 = e => {
         window.scrollTo(0, 1055);
-        setState({ [e.target.id]: true })
     }
 
 
     return (
         <NavContainer>
-            <div>
-                <button onClick={e => setOpen(!open)}>Menu</button>
-            </div>
-            <div className={`links ${open && 'active'}`}>
-                <h4 id='about' onClick={handleScroll2}>
-                    {nav_text.about}
-                </h4>
-                <h4 id='skills' onClick={handleScroll3}>
-                    {nav_text.skills}
-                </h4>
-                <h4 id='proyects' onClick={handleScroll4}>
-                    {nav_text.proyects}
-                </h4>
-                <h4 id='contact' onClick={handleScroll}>
-                    {nav_text.contact}
-                </h4>
+            <div className={`bigBox ${open && 'active'}`}>
+                <div className={`menu ${open && 'active'}`}>
+                    <MenuIcon setState={setOpen} state={open}/>
+                </div>
+
+                <div className={`links ${open && 'active'}`}>
+                    <h4 id='about' onClick={handleScroll2}>
+                        {nav_text.about}
+                    </h4>
+                    <h4 id='skills' onClick={handleScroll3}>
+                        {nav_text.skills}
+                    </h4>
+                    <h4 id='proyects' onClick={handleScroll4}>
+                        {nav_text.proyects}
+                    </h4>
+                    <h4 id='contact' onClick={handleScroll}>
+                        {nav_text.contact}
+                    </h4>
+                </div>
+
+                <div className={`box ${open && 'active'}`}>
+                    <div>
+                        <img src={localStorage.theme === 'primary' ? sun_logo : moon_logo} alt='error' onClick={handleClick} />
+                    </div>
+                    <select name="language" id="" onChange={handleSelect}>
+                        <option value="en">EN</option>
+                        <option value="esp">ESP</option>
+                    </select>
+                </div>
             </div>
 
-            <div className='box'>
-                <div>
-                    <img src={localStorage.theme === 'primary' ? sun_logo : moon_logo} alt='error' onClick={handleClick} />
-                </div>
-                <select name="language" id="" onChange={handleSelect}>
-                    <option value="en">EN</option>
-                    <option value="esp">ESP</option>
-                </select>
-            </div>
 
         </NavContainer>
     )
